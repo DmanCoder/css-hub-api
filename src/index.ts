d3218392e13c0efd624600f4ab51a6dcd79b0aad
DmanCoder <ktdenis.1@live.com>
2022-12-12 21:31:20 +1000

    to ts

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import app from './app.js';
+import config from './config.js';
+
+app.listen(config.port, () => {
+  console.log(`🚀 ${config.name} ${config.version} 🚀`);
+  console.log(
+    `🚀 Listening on ${config.port} with NODE_ENV=${config.nodeEnv} 🚀`
+  );
+});
